a824ed54e5cc119e1543488337f1e9474269c6a1
Jeff Tang <mrjefftang@users.noreply.github.com>
2022-10-20 19:30:19 -0400

    update klipy log

--- klipper/docker-entrypoint.sh
+++ klipper/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 /srv/klipper/linux_mcu &
-/srv/venv/bin/python klipper/klippy/klippy.py -I ${KLIPPER_TTY} -a ${KLIPPER_API} ${KLIPPER_CFG}
+/srv/venv/bin/python klipper/klippy/klippy.py -I ${KLIPPER_TTY} -a ${KLIPPER_API} -l ${KLIPPER_LOG} ${KLIPPER_CFG}
